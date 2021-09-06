77d861eba34cb38d036ad98880e371f6099cb812
abhaygedam <abhaygedam1999@gmail.com>
2021-09-06 21:15:13 +0530

    React Assignemnt 1

---
+++ react-mobile/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Static from "./component/static"
+
+function App() {
+  return (
+    <div style={{
+            marginLeft:"35%",
+        }}>
+       <Static />
+    </div>
+  );
+}
+
+export default App;
